486129392121208b18a6164135d73c759e904067
Gonzalonoe <gonza2002noe@gmail.com>
2025-10-08 15:07:21 -0300

    1.0.5

--- app/src/main/java/com/example/tpinmobiliariappdmgn/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/tpinmobiliariappdmgn/ui/login/LoginActivity.java
@@ -2,44 +2,42 @@ package com.example.tpinmobiliariappdmgn.ui.login;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.tpinmobiliariappdmgn.R;
 import com.example.tpinmobiliariappdmgn.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private LoginActivityViewModel viewModel;
+    private LoginActivityViewModel vm;
     private ActivityLoginBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        vm = ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication()).create(LoginActivityViewModel.class);
         setContentView(binding.getRoot());
+        vm.getMMensaje().observe(this, new Observer<String>() {
+                    @Override
+                    public void onChanged(String s) {
+                        Toast.makeText(LoginActivity.this, s, Toast.LENGTH_SHORT).show();
 
-        viewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication()).create(LoginActivityViewModel.class);
-
-        viewModel.getErrorMutableLiveData().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(String s) {
-                binding.tvError.getText();
-            }
-        });
+                    }
+                });
 
         binding.btLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              String email = binding.etEmail.getText().toString();
-              String password = binding.etPassword.getText().toString();
-              viewModel.validarUsuario(email, password);
+                String usu = binding.etEmail.getText().toString();
+                String contra = binding.etPassword.getText().toString();
+                vm.logueo(usu, contra);
             }
         });
     }
 }
+
+
--- app/src/main/java/com/example/tpinmobiliariappdmgn/ui/login/LoginActivityViewModel.java
+++ app/src/main/java/com/example/tpinmobiliariappdmgn/ui/login/LoginActivityViewModel.java
@@ -3,6 +3,7 @@ package com.example.tpinmobiliariappdmgn.ui.login;
 import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
@@ -10,49 +11,66 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
 import com.example.tpinmobiliariappdmgn.MainActivity;
-import com.example.tpinmobiliariappdmgn.R;
-import com.example.tpinmobiliariappdmgn.ui.models.Usuario;
+import com.example.tpinmobiliariappdmgn.request.ApiClient;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LoginActivityViewModel extends AndroidViewModel {
 
     private Context context;
-    private MutableLiveData<String> userMutable;
-    private MutableLiveData<String> errorMutable;
+    private MutableLiveData<String> mMensaje;
+
     public LoginActivityViewModel(@NonNull Application application) {
         super(application);
         context = getApplication();
     }
 
-    public LiveData<String> getUserMutableLiveData(){
-        if (userMutable == null){
-            userMutable = new MutableLiveData<>();
+    public LiveData<String> getMMensaje() {
+        if (mMensaje == null) {
+            mMensaje = new MutableLiveData<>();
         }
-        return userMutable;
-    }
-    public LiveData<String> getErrorMutableLiveData(){
-        if (errorMutable == null){
-            errorMutable = new MutableLiveData<>();
-        }
-        return errorMutable;
+        return mMensaje;
     }
 
-    public void validarUsuario(String email, String password){
-        if (email.isEmpty() || password.isEmpty()) {
-            errorMutable.setValue("Todos los campos son obligatorios");
+    public void logueo(String usuario, String contrasenia) {
+        if (usuario.isEmpty() || contrasenia.isEmpty()) {
+            mMensaje.setValue("Error, campos vacíos");
             return;
         }
+        ApiClient.InmoServicio inmoServicio = ApiClient.getInmoServicio();
+        Call<String> call = inmoServicio.loginForm(usuario, contrasenia);
+        call.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                if (response.isSuccessful()) {
+                    String token = response.body();
+                    ApiClient.guardarToken(getApplication(), token);
+                    Log.d("token", token);
+                    Intent intent = new Intent(getApplication(), MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    getApplication().startActivity(intent);
+                } else {
+                    Log.d("token", response.message());
+                    Log.d("token", response.code() + "");
+                    Log.d("token", response.errorBody() + "");
+                }
+            }
 
-        if ( email.equals("Gonza") && password.equals("12345")) {
-            Usuario usuario = new Usuario();
-            usuario.setEmail(email);
-            usuario.setPassword(password);
-            usuario.setNombre("Gonzalo");
-            usuario.setFoto(R.drawable.foto1);
+            @Override
+            public void onFailure(Call<String> call, Throwable throwable) {
+                Log.d("token", throwable.getMessage());
 
-            Intent intent = new Intent(getApplication(), MainActivity.class);
-            intent.putExtra("User", usuario);
-            intent.setFlags(intent.FLAG_ACTIVITY_NEW_TASK);
-            getApplication().startActivity(intent);
-        }
+            }
+        });
     }
 }
+
+
+
+
+
+
+
+
